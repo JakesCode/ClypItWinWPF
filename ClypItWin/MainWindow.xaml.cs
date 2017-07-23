f0e6a2d0d7d291973cd25f0dd2e3f87b244ab7e7
JakesCode <stringcraftgaming@gmail.com>
2017-07-23 17:34:38 +0100

    developing notifications

--- ClypItWin/MainWindow.xaml.cs
+++ ClypItWin/MainWindow.xaml.cs
@@ -31,7 +31,8 @@ namespace ClypItWin
         {
             InitializeComponent();
         }
-        private string ClypQuery(string Url, Method HttpMethod, Dictionary<string, string> Headers, Dictionary<string, string> Parameters)
+
+        public static string ClypQuery(string Url, Method HttpMethod, Dictionary<string, string> Headers, Dictionary<string, string> Parameters, Dictionary<string, string> URLParameters)
         {
             var client = new RestClient(Url);
             var request = new RestRequest(HttpMethod);
@@ -43,9 +44,26 @@ namespace ClypItWin
             {
                 request.AddParameter(parameter.Key, parameter.Value);
             }
+            foreach (var urlParameter in URLParameters)
+            {
+                request.AddUrlSegment(urlParameter.Key, urlParameter.Value);
+            }
             IRestResponse response = client.Execute(request);
             return response.Content;
 
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void notificationBell_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Notifications notificationsWindow = new Notifications(this.Clyp);
+            notificationsWindow.Top = this.Top;
+            notificationsWindow.Left = this.Left;
+            notificationsWindow.ShowDialog();
         }
 
         private void settingsButton_Click(object sender, RoutedEventArgs e)
@@ -63,13 +81,28 @@ namespace ClypItWin
                 new Dictionary<string, string>()
                 { { "Authorization", "Basic MjkzMTE5Og==" }, { "Content-Type", "application/x-www-form-urlencoded" } },
                 new Dictionary<string, string>()
-                { { "grant_type", "password" }, { "username", settingsWindow.usernameTextbox.Text }, { "password", settingsWindow.passwordTextbox.Password.ToString() } }));
+                { { "grant_type", "password" }, { "username", settingsWindow.usernameTextbox.Text }, { "password", settingsWindow.passwordTextbox.Password.ToString() } },
+                new Dictionary<string, string>()));
                 Clyp.user = JsonConvert.DeserializeObject<ClypUser>(ClypQuery("https://api.clyp.it/me",
                     Method.GET,
                     new Dictionary<string, string>()
                     { { "authorization", "Bearer " + Clyp.access_token }, { "Content-Type", "application/x-www-form-urlencoded" }, { "x-client-type", "WebAlfa" } },
+                    new Dictionary<string, string>(),
                     new Dictionary<string, string>()));
                 username.Content = "Logged in as: " + Clyp.user.FirstName;
+
+                if(Clyp.user.NotificationsSummary.Count > 0)
+                {
+                    BitmapImage unreadNotificationsBell = new BitmapImage();
+                    unreadNotificationsBell.BeginInit();
+                    unreadNotificationsBell.UriSource = new Uri("pack://application:,,,/ClypItWin;component/assets/notification_unread.png");
+                    unreadNotificationsBell.EndInit();
+                    notificationBell.Source = unreadNotificationsBell;
+                    notificationBell.IsEnabled = true;
+                    notificationsCircle.Visibility = Visibility.Visible;
+                    numberOfNotifications.Visibility = Visibility.Visible;
+                    numberOfNotifications.Content = Clyp.user.NotificationsSummary.Count;
+                }
 
                 while (Clyp.access_token == null && Clyp.login_attempt)
                 {
@@ -81,10 +114,10 @@ namespace ClypItWin
 
         public class ClypSession
         {
+            public bool login_attempt = false;
             public string access_token { get; set; }
             public int expires_in { get; set; }
             public string refresh_token { get; set; }
-            public bool login_attempt = false;
             public string token_type { get; set; }
             public ClypUser user { get; set; }
         }
@@ -106,20 +139,11 @@ namespace ClypItWin
             public double UploadSecondsRemaining { get; set; }
             public string UserId { get; set; }
             public string Website { get; set; }
+
             public class NotificationsSummaryClass
             {
-                public int Count { get; set; }
+                public int Count = 0;
             }
-        }
-
-        private void closeButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void notificationBell_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            System.Windows.Forms.MessageBox.Show("Test");
         }
     }
 }
--- ClypItWin/Notifications.xaml.cs
+++ ClypItWin/Notifications.xaml.cs
@@ -10,9 +10,13 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using RestSharp;
 using System.Threading;
-
+using System.Net;
 namespace ClypItWin
 {
     /// <summary>
@@ -20,11 +24,104 @@ namespace ClypItWin
     /// </summary>
     public partial class Notifications : Window
     {
-        public MainWindow.ClypSession Clyp = new MainWindow.ClypSession();
-
-        public Notifications()
+        public Notifications(MainWindow.ClypSession Clyp)
         {
             InitializeComponent();
+            if(Clyp.access_token == null)
+            {
+                notLoggedInLabel.Visibility = Visibility.Visible;
+            } else
+            {
+                notLoggedInLabel.Visibility = Visibility.Hidden;
+                if(Clyp.user.NotificationsSummary.Count > 0)
+                {
+                    ClypNotifications Notifications = JsonConvert.DeserializeObject<ClypNotifications>(MainWindow.ClypQuery("https://api.clyp.it/me/notifications",
+                        Method.GET,
+                        new Dictionary<string, string>()
+                        { { "authorization", "Bearer " + Clyp.access_token }, { "Content-Type", "application/x-www-form-urlencoded" }, { "x-client-type", "WebAlfa" } },
+                        new Dictionary<string, string>(),
+                        new Dictionary<string, string>()
+                        { { "count", "5" } }));
+                    createNotifications(Notifications);
+                }
+                
+            }
+
+        }
+
+        private void createNotifications(ClypNotifications Notifications)
+        {
+            int notificationNumber = 0;
+
+            foreach (var notification in Notifications.Data)
+            {
+                Grid container = new Grid();
+                container.Height = 42;
+                container.Width = 260;
+                if(notificationNumber > 0)
+                {
+                    container.Margin = new Thickness() { Top = 42 * notificationNumber };
+                } else
+                {
+                    container.Margin = new Thickness() { Top = 0 };
+                }
+                notificationsCanvas.Children.Add(container);
+
+                Rectangle backgroundBox = new Rectangle();
+                backgroundBox.Fill = Brushes.White;
+                backgroundBox.Height = container.Height;
+                backgroundBox.Width = container.Width;
+                container.Children.Add(backgroundBox);
+
+                Border blackBorder = new Border();
+                blackBorder.BorderBrush = Brushes.Black;
+                blackBorder.BorderThickness = new Thickness(1);
+                blackBorder.Height = container.Height - 1;
+                blackBorder.Width = container.Width - 1;
+                container.Children.Add(blackBorder);
+
+                TextBlock details = new TextBlock();
+                details.VerticalAlignment = VerticalAlignment.Center;
+                details.FontFamily = new FontFamily("Raleway");
+                details.FontSize = 13;
+                details.Foreground = Brushes.Black;
+                if(notification.Text.Length > 40)
+                {
+                    details.Text = notification.Text.Substring(0, 37) + "...";
+                } else
+                {
+                    details.Text = notification.Text;
+                }
+                container.Children.Add(details);
+
+                notificationNumber++;   
+            }
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public class Datum
+        {
+            public string NotificationId { get; set; }
+            public string DateCreated { get; set; }
+            public bool Acknowledged { get; set; }
+            public string ImageUrl { get; set; }
+            public string Text { get; set; }
+            public string TargetUrl { get; set; }
+        }
+
+        public class Paging
+        {
+        }
+
+        public class ClypNotifications
+        {
+            public List<Datum> Data { get; set; }
+            public int TotalCount { get; set; }
+            public Paging Paging { get; set; }
         }
     }
 }
